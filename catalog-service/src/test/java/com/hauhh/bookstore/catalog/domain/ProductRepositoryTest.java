507594995a111dbde53f776ec14da78f86b79dd6
HauHH1910 <hhhau1910@gmail.com>
2025-05-12 20:21:12 +0700

    Unit Test Product Controller and Product Repository

--- catalog-service/src/test/java/com/hauhh/bookstore/catalog/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/hauhh/bookstore/catalog/domain/ProductRepositoryTest.java
@@ -5,36 +5,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.hauhh.bookstore.catalog.ContainerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+@DataJpaTest
 @Sql("/test-data.sql")
-@DataJpaTest(properties = {
-    "spring.test.database.replace=none",
-    "spring.datasource.url=jdbc:tc:postgresql:lastest:///db"
-})
+@Import(ContainerConfig.class)
 class ProductRepositoryTest {
 
-  @Autowired
-  private ProductRepository productRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
-  @Test
-  void shouldGetAllProducts() {
-    List<ProductEntity> products = productRepository.findAll();
-    assertThat(products).hasSize(15);
-  }
+    @Test
+    void shouldGetAllProducts() {
+        List<ProductEntity> products = productRepository.findAll();
+        assertThat(products).hasSize(15);
+    }
 
-  @Test
-  void shouldGetProductByCode() {
-    ProductEntity product = productRepository.findByCode("P100").orElseThrow();
-    assertThat(product.getCode()).isEqualTo("P100");
-    assertThat(product.getName()).isEqualTo("The Hunger Games");
-    assertThat(product.getDescription()).isEqualTo("Winning will make you famous. Losing means certain death...");
-    assertThat(product.getPrice()).isEqualTo(new BigDecimal("34.0"));
-  }
+    @Test
+    void shouldGetProductByCode() {
+        ProductEntity product = productRepository.findByCode("P100").orElseThrow();
+        assertThat(product.getCode()).isEqualTo("P100");
+        assertThat(product.getName()).isEqualTo("The Hunger Games");
+        assertThat(product.getDescription()).isEqualTo("Winning will make you famous. Losing means certain death...");
+        assertThat(product.getPrice()).isEqualTo(new BigDecimal("34.0"));
+    }
+
+    @Test
+    void shouldReturnEmptyWhenProductCodeNotExists() {
+        assertThat(productRepository.findByCode("invalid_product_code")).isEmpty();
+    }
 
 }
